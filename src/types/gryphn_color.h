92ef68938ac1ebd6ad7f341b968053986c3b40b7
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-05-14 05:59:06 -0400

    Add c++ compatible struct

--- src/types/gryphn_color.h
+++ src/types/gryphn_color.h
@@ -1,6 +1,46 @@
 #include "../math/gryphn_vec4.h"
 
+#ifdef GN_UTILS_CPP
+struct gnColor {
+public:
+    union {
+        struct {
+            int r, g, b;
+            float a;
+        };
+
+        struct {
+            int red, green, blue;
+            float alpha;
+        };
+    };
+public:
+    gnColor(int red, int green, int blue, float alpha = 1.0) {
+        this->red = red;
+        this->green = green;
+        this->blue = blue;
+        this->alpha = alpha;
+    }
+
+    gnColor(int color, float alpha = 1.0) {
+        this->red = color;
+        this->green = color;
+        this->blue = color;
+        this->alpha = alpha;
+    }
+};
+#else
 typedef struct gnColor {
-    int r, g, b;
-    float a;
+    union {
+        struct {
+            int r, g, b;
+            float a;
+        };
+
+        struct {
+            int red, green, blue;
+            float alpha;
+        };
+    };
 } gnColor;
+#endif
