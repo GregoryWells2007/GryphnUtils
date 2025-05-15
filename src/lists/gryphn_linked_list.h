1d25f4d94b3c8a9482ec2023fd7294d1fd72cc6d
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-05-15 18:50:24 -0400

    basic gnCreateLinkedList

--- src/lists/gryphn_linked_list.h
+++ src/lists/gryphn_linked_list.h
@@ -1,7 +1,22 @@
 #pragma once
+#include "stdlib.h"
 
 // why would one use a linked list
 typedef struct gnLinkedList {
     void* data;
     gnLinkedList* nextNode;
 } gnLinkedList;
+
+static gnLinkedList gnCreateLinkedList(int count) {
+    gnLinkedList list;
+
+    gnLinkedList* currentNode = &list;
+    for (int i = 0; i < count; i++) {
+        if ((i + 1) < count) {
+            currentNode->nextNode = (gnLinkedList*)malloc(sizeof(gnLinkedList));
+            currentNode = currentNode->nextNode;
+        }
+    }
+
+    return list;
+}
