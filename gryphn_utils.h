5395e326cb15dd8ec176fa6feb7d550c04388353
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-06-06 20:15:18 -0400

    make it work

--- gryphn_utils.h
+++ gryphn_utils.h
@@ -3,15 +3,15 @@
 #include "stdint.h"
 #include "stdlib.h"
 
-#include "src/gryphn_bool.h"
-#include "src/gryphn_error_code.h"
-#include "src/gryphn_version.h"
-#include "src/math/gryphn_vec2.h"
-#include "src/math/gryphn_vec3.h"
-#include "src/math/gryphn_vec4.h"
-#include "src/gryphn_color.h"
-#include "src/gryphn_color_format.h"
-#include "src/gryphn_image_format.h"
+#include "utils/gryphn_bool.h"
+#include "utils/gryphn_error_code.h"
+#include "utils/gryphn_version.h"
+#include "utils/math/gryphn_vec2.h"
+#include "utils/math/gryphn_vec3.h"
+#include "utils/math/gryphn_vec4.h"
+#include "utils/gryphn_color.h"
+#include "utils/gryphn_color_format.h"
+#include "utils/gryphn_image_format.h"
 
 typedef uint32_t gnUInt;
 typedef char gnByte;
