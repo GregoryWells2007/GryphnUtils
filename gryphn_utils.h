d8d0ac26242261b9dfc96e90deb4da46cf78508d
Gregory Wells <me@gregorywells.dev>
2025-07-28 20:07:05 -0400

    update utils include

--- gryphn_utils.h
+++ gryphn_utils.h
@@ -1,24 +1,14 @@
 #pragma once
 
-#include "stdint.h"
-#include "stdlib.h"
-
-#include "utils/gryphn_bool.h"
-#include "utils/gryphn_error_code.h"
-#include "utils/gryphn_version.h"
-#include "utils/math/gryphn_vec2.h"
-#include "utils/math/gryphn_vec3.h"
-#include "utils/math/gryphn_vec4.h"
-#include "utils/math/gryphn_mat4.h"
-#include "utils/math/gryphn_math.h"
-#include "utils/gryphn_color.h"
-#include "utils/gryphn_color_format.h"
-#include "utils/gryphn_image_format.h"
-#include "utils/lists/gryphn_array_list.h"
-
-typedef uint32_t gnUInt;
-typedef char gnByte;
-typedef unsigned char gnUByte;
-typedef int gnInt;
-typedef float gnFloat;
-typedef size_t gnSize;
+#include <utils/gryphn_bool.h>
+#include <utils/gryphn_error_code.h>
+#include <utils/gryphn_version.h>
+#include <utils/math/gryphn_vec2.h>
+#include <utils/math/gryphn_vec3.h>
+#include <utils/math/gryphn_vec4.h>
+#include <utils/math/gryphn_mat4.h>
+#include <utils/math/gryphn_math.h>
+#include <utils/gryphn_color.h>
+#include <utils/gryphn_color_format.h>
+#include <utils/gryphn_image_format.h>
+#include <utils/lists/gryphn_array_list.h>
