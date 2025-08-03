ccfbc896b377972eafa32b581b44a9cafd1761ac
Gregory Wells <me@gregorywells.dev>
2025-08-03 11:47:38 -0400

    forgot something

--- gryphn_utils.h
+++ gryphn_utils.h
@@ -8,5 +8,4 @@
 #include <utils/math/gryphn_mat4.h>
 #include <utils/math/gryphn_math.h>
 #include <utils/gryphn_color.h>
-#include <utils/gryphn_image_format.h>
 #include <utils/lists/gryphn_array_list.h>
